ecc66b3acf87c2d7a300ebf3daa884dfef2280e9
Nitish <nk12889@gmail.com>
2024-03-07 08:00:25 +0530

    connecting to databse

---
+++ util/database.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2');
+
+
+const pool = mysql.createPool({
+     host : 'localhost',
+     user : 'root',
+     database: 'user-data',
+     password:'705043'
+
+})
+
+module.exports = pool.promise();
